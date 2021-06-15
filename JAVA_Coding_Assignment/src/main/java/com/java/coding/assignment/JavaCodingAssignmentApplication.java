5f1a5ea252ef8bd95cc07d81b9c570b2376fdf39
dipak_inexture <dipakprajapati.inexture@gmail.com>
2021-06-15 14:52:09 +0530

    create project

---
+++ JAVA_Coding_Assignment/src/main/java/com/java/coding/assignment/JavaCodingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.java.coding.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingAssignmentApplication.class, args);
+	}
+
+}
---
+++ JAVA_Coding_Assignment/src/test/java/com/java/coding/assignment/JavaCodingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.coding.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
