f42ff8a9d011b0d21c0fb29e5862c532c5009722
shreyansh <shreyansh.soni@prospecta.com>
2021-06-15 19:56:53 +0530

    remove unused library

--- JAVA_Coding_Assignment/src/test/java/com/java/coding/assignment/controller/AssignmentControllerTest.java
+++ JAVA_Coding_Assignment/src/test/java/com/java/coding/assignment/controller/AssignmentControllerTest.java
@@ -1,18 +1,14 @@
 package com.java.coding.assignment.controller;
 
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
